4a404f3cc23bb5bcf0ff600d817244455f627f0c
Benoît Hubert <benoithubert@gmail.com>
2017-01-26 11:19:47 +0100

    Yep we got a commit array

--- seqLoop.js
+++ seqLoop.js
@@ -1,20 +1,25 @@
 // Source: http://jsfiddle.net/dukeytoo/02ohnth4/
 
 const Git = require("nodegit");
-const repo = "/Library/WebServer/Documents/localhost/zerobin/.git";
+const repo = __dirname + "/.git"; // "/Library/WebServer/Documents/localhost/zerobin/.git";
 
 Git.Repository.open(repo)
 .then(repository => {
   return repository.getBranchCommit("master");
 })
-.then(seqLoopDescending);
+.then(commit => seqLoopDescending(commit, [commit]))
+.then(allCommits => {
+    // console.log('all commits', allCommits);
+    allCommits.forEach(commit => console.log(commit.message()));
+})
+.catch(console.err);
 
 
 /** 
  * Fake random async work.  Returns (input + i + " ")
  **/
 function doTheWork(commit) {
-    console.log(commit.message());
+    // console.log(commit.message());
     return commit.getParents()
     .then(commits => {
         return commits[0];
@@ -29,10 +34,13 @@ function doTheWork(commit) {
  * This is the simplest variant, but it outputs the indexes in descending order.  Use it if your work 
  * function does not care about the index, or only needs to know when its on the last item.
  **/
-function seqLoopDescending(someCommit) {
+function seqLoopDescending(someCommit, allCommits) {
+    // console.log(allCommits);
     return new Promise(function (resolve, reject) {
         doTheWork(someCommit).then(function (parentCommit) {
-            resolve(seqLoopDescending(parentCommit));
+            // console.log('parent', parentCommit);
+            if(parentCommit === undefined) resolve(allCommits);
+            else resolve(seqLoopDescending(parentCommit, allCommits.concat([parentCommit])));
         })
     });
 }
